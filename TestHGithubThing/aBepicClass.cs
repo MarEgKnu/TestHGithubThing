ee4b46d49b20b566db280c8d3f676c49ff0ac119
Magnus-Hansen <54136904+Magnus-Hansen@users.noreply.github.com>
2023-11-27 10:40:09 +0100

    Added an amazing class, very gamer

---
+++ TestHGithubThing/aBepicClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHGithubThing
+{
+    public class aBepicClass
+    {
+        public string FemboyHooters { get; set; }
+    }
+}
